f24a5f9449639e7c60fc54ff1d35a0802eaad99b
probonopd <probono>
2015-11-22 20:43:00 +0100

    debian

--- boot/bin/detect_debian_live.sh
+++ boot/bin/detect_debian_live.sh
@@ -10,6 +10,7 @@
 # debian-live-7.5-i386-gnome-desktop+nonfree.iso - live-boot 3.0.1-1
 # debian_7.0.0_wheezy_i386_20130705_binary.hybrid.iso - live-boot 3.0.1-1
 # debian-live-8.0.0-amd64-xfce-desktop+nonfree.iso - live-boot 4.0.2-1
+# tails-i386-1.5.iso
 
 detect_debian_live() {
 
@@ -35,6 +36,11 @@ if [ -f "$MOUNTPOINT"/usr/share/doc/live-boot/changelog.Debian.gz ] ; then
   echo "* LIVETOOL $LIVETOOL"
   LIVETOOLVERSION=$(zcat "$MOUNTPOINT"/usr/share/doc/live-boot/changelog.Debian.gz | head -n 1 | cut -d "(" -f 2 | cut -d ")" -f 1)
   echo "* LIVETOOLVERSION $LIVETOOLVERSION"
+else
+  LIVETOOL="debian-live"
+  echo "* LIVETOOL $LIVETOOL"
+  LIVETOOLVERSION=0
+  echo "* LIVETOOLVERSION $LIVETOOLVERSION"
 fi
 
 umount "$MOUNTPOINT"
@@ -42,12 +48,18 @@ umount "$MOUNTPOINT"
 CFG=$(find "$MOUNTPOINT" -name live.cfg | head -n 1)
 
 LINUX=$(cat $CFG | grep "linux " | head -n 1 | sed -e 's|linux ||g' | xargs)
+if [ "$LINUX" == "" ] ; then
+  LINUX=$(cat $CFG | grep "kernel " | head -n 1 | sed -e 's|kernel ||g' | xargs) # tails-i386-1.5.iso
+fi
 echo "* LINUX $LINUX"
 
 INITRD=$(cat $CFG | grep "initrd " | head -n 1 | sed -e 's|initrd ||g' | xargs)
+if [ "$INITRD" == "" ] ; then
+  INITRD=$(cat $CFG | grep "initrd=" | head -n 1  | cut -d = -f 2 | cut -d " " -f 1) # tails-i386-1.5.iso
+fi
 echo "* INITRD $INITRD"
 
-APPEND=$(cat $CFG | grep "append " | head -n 1 | sed -e 's|append ||g' | xargs)
+APPEND=$(cat $CFG | grep "append " | head -n 1 | sed -e 's|append ||g' | xargs | sed -e 's|initrd='$INITRD' ||g') # sed is for tails-i386-1.5.iso
 echo "* APPEND $APPEND"
 
 #
